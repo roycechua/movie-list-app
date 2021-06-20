301d53e0bc7ce4d98ed0b319d7de6d3a82c348e6
Royce Chua <royce236@gmail.com>
2021-06-21 00:55:40 +0800

    Add react query client

--- App.tsx
+++ App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import MainNavigationContainer from './src/navigation/MainNavigationContainer';
 import { Provider as StoreProvider } from 'react-redux';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { store } from './src/redux/store';
 import theme from './src/themes/themes';
 
-const App : React.FC = () => {
+const queryClient = new QueryClient();
 
+const App: React.FC = () => {
 	return (
 		<StoreProvider store={store}>
 			<PaperProvider theme={theme}>
-				<MainNavigationContainer/>
+				<QueryClientProvider client={queryClient}>
+					<MainNavigationContainer />
+				</QueryClientProvider>
 			</PaperProvider>
 		</StoreProvider>
 	);
-}
+};
 
 export default App;
