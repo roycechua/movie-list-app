319a6664a2a505538751ef83d751c11f28416da5
Royce Chua <royce236@gmail.com>
2021-06-20 19:50:06 +0800

    Add themes file

---
+++ src/themes/themes.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'react-native-paper';
+
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#032541',
+      accent: '#10B6DE',
+    },
+};
+
+export default theme;
