03a2bc6bcba94351e5ec7a8af78c5f20264e6ba3
Royce Chua <royce236@gmail.com>
2021-06-21 00:55:52 +0800

    Add logout component to header

--- src/navigation/MainNavigationContainer.tsx
+++ src/navigation/MainNavigationContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from '../common/components/SplashScreen';
@@ -10,6 +10,9 @@ import MovieDetailScreen from '../features/movies/MovieDetailScreen';
 import WatchListScreen from '../features/watchlist/WatchListScreen';
 import { fetchConfiguration } from '../features/config/configSlice';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { useTheme } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { setLogout } from '../features/auth/authSlice';
 
 export type RootStackParamsList = {
 	SignIn: undefined;
@@ -24,26 +27,32 @@ type Props = {};
 const Stack = createStackNavigator();
 
 const MainNavigationContainer: React.FC<Props> = (props: Props) => {
+	const { colors } = useTheme();
+
 	// to be replaced later by redux state variables
 	const [isSignout, setIsSignout] = useState(false);
 
 	const dispatch = useAppDispatch();
-	const isLoggedIn = useAppSelector(state => state.auth.isLoggedIn);
-	const isAttemptingFetchConfiguration = useAppSelector(state => state.config.isAttemptingFetchConfiguration);
+	const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
+	const isAttemptingFetchConfiguration = useAppSelector(
+		(state) => state.config.isAttemptingFetchConfiguration
+	);
 
 	useEffect(() => {
-		dispatch(fetchConfiguration())
-	}, [])
+		dispatch(fetchConfiguration());
+	}, []);
 
 	if (isAttemptingFetchConfiguration) {
 		// We haven't finished checking for the token yet
 		return <SplashScreen />;
 	}
 
+	const handleLogout = () => dispatch(setLogout());
+
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
-				{isLoggedIn ? (
+				{!isLoggedIn ? (
 					<>
 						{/* No token found, user isn't signed in */}
 						<Stack.Screen
@@ -72,7 +81,30 @@ const MainNavigationContainer: React.FC<Props> = (props: Props) => {
 				) : (
 					<>
 						{/* User is signed in */}
-						<Stack.Screen name='Home' component={HomeScreen} />
+						<Stack.Screen
+							name='Home'
+							component={HomeScreen}
+							options={{
+								headerStyle: {
+									backgroundColor: colors.primary,
+								},
+								headerTitleStyle: {
+									color: 'white',
+									fontWeight: 'bold',
+								},
+								headerRight: () => (
+									<TouchableOpacity onPress={handleLogout}>
+										<View style={{margin: 10}}>
+											<MaterialCommunityIcons
+												name='logout'
+												size={24}
+												color='white'
+											/>
+										</View>
+									</TouchableOpacity>
+								),
+							}}
+						/>
 						<Stack.Screen
 							name='MovieDetail'
 							component={MovieDetailScreen}
