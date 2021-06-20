faea5e0a01fb30d29593ef5548bd28db9d0b9426
Royce Chua <royce236@gmail.com>
2021-06-21 00:55:13 +0800

    Add new API endpoints for HomeScreen functions

--- src/services/API.ts
+++ src/services/API.ts
@@ -17,4 +17,6 @@ export default {
     getAPIConfiguration: () => AxiosModule.get(`/configuration?api_key=${constants.API_KEY}`),
 
     // Home
+	getTrendingMovies: () => AxiosModule.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${constants.API_KEY}`),
+	searchMovies: (payload: any) => AxiosModule.get(`https://api.themoviedb.org/3/search/movie?query=${payload.query}&language=en-US&api_key=${constants.API_KEY}`),
 };
