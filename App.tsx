ed1ca75dc872c1fa2ab577135054cecdb5a5bcb8
Royce Chua <royce236@gmail.com>
2021-06-20 19:49:54 +0800

    Add paper provider and themes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import MainNavigationContainer from './src/navigation/MainNavigationContainer';
 import { Provider as StoreProvider } from 'react-redux';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { store } from './src/redux/store';
+import theme from './src/themes/themes';
 
 const App : React.FC = () => {
+
 	return (
 		<StoreProvider store={store}>
-			<MainNavigationContainer/>
+			<PaperProvider theme={theme}>
+				<MainNavigationContainer/>
+			</PaperProvider>
 		</StoreProvider>
 	);
 }
