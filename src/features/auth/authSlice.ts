49e4c54415714b5f9f437ce7a84c5419840d0428
Royce Chua <royce236@gmail.com>
2021-06-21 19:25:49 +0800

    Add API calls necessary to access watch list APIs

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,9 +1,16 @@
 import { createSlice, Dispatch } from '@reduxjs/toolkit';
 import API from '../../services/API';
-import { setRequestToken } from '../config/configSlice';
+import { setRequestToken, setUserData } from '../user/userSlice';
 import { loginPayload } from './types';
 
-const initialState = {
+type initialAuthState = {
+	isLoggedIn: boolean,
+	attemptingLogin: boolean,
+	loginSuccess: boolean,
+	loginFailed: boolean,
+};
+
+const initialState : initialAuthState = {
 	isLoggedIn: false,
 	attemptingLogin: false,
 	loginSuccess: false,
@@ -52,17 +59,35 @@ export const login = (payload: loginPayload) => async (dispatch: Dispatch) => {
 		if (requestTokenRes.status != 200)
 			throw new Error('Get Request Token Failed');
 
+		// assign API data to a binding
+		const request_token = requestTokenRes.data.request_token;
+
 		// save request token to avoid redundant API calls
-		dispatch(setRequestToken(requestTokenRes.data.request_token));
+		dispatch(setRequestToken(request_token));
 
 		// perform login POST request
 		let loginRes = await API.login({
 			...payload,
-			request_token: requestTokenRes.data.request_token,
+			request_token,
 		});
 
 		if (loginRes.status != 200) throw new Error('Login Failed');
 
+		// create session to access user data and user data API functions
+		let createSessionRes = await API.createSession({ request_token });
+
+		if (createSessionRes.status != 200) throw new Error('Create Session Failed');
+
+		// assign session id API data to a binding
+		const session_id = createSessionRes.data.session_id;
+
+		// get user account details
+		let accountDetailsRes = await API.getAccountDetails({ session_id });
+
+		if (accountDetailsRes.status != 200) throw new Error('Get Account Details Failed');
+
+		// set user data on a redux state with session_id
+		dispatch(setUserData({...accountDetailsRes.data, sessionId: session_id }))
 		dispatch(setLoginSuccess());
 	} catch (error) {
 		alert(`Login Failed. '${error.message}'`);
